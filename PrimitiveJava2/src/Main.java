0aa365710e5c84baf753b36dac40165a1a8ff580
Anton Borodin <bor183@mail.ru>
2025-09-22 17:16:55 +0500

    add main

--- PrimitiveJava2/src/Main.java
+++ PrimitiveJava2/src/Main.java
@@ -1,3 +1,27 @@
 public class Main {
-    
+    public static void main(String[] args) {
+
+        // Объявляете переменные для входных данных и
+        // параметров программы: начального счёта,
+        // суммы пополнения и тп
+
+        // Условным оператором проверяете, превысила ли
+        // сумма пополнения порог, и для этих двух разных
+        // сценариев рассчитываете сумму бонуса и выводите
+        // на экран.
+        int balance = 100;
+        int amound = 1050;
+        int bonus = 0;
+        int finalBalance;
+
+        if (amound > 1000) {
+            bonus = amound / 100;
+        }
+        finalBalance = balance + amound + bonus;
+
+        System.out.println("Бонусные рубли:" + " " + bonus);
+        System.out.println("Итоговый счет:" + " " + finalBalance);
+
+
+    }
 }
